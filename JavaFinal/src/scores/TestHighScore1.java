6f40402cb890e48ac001cd6630b541c01c272efd
Leesanwei <lswei93@hotmail.com>
2016-04-23 04:13:17 +0200

    test

---
+++ JavaFinal/src/scores/TestHighScore1.java
@@ -0,0 +1,18 @@
+package scores;
+
+import java.util.*;
+import java.io.*;
+
+public class TestHighScore1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Please write your player name below : ");
+		Scanner sc = new Scanner(System.in);
+		String namePlayer = sc.nextLine();
+		sc.close();
+		System.out.println(namePlayer);
+
+	}
+
+}
