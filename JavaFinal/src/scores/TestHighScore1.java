a681c66fd5df657707b4a17195fd9cecd5ea9fc7
Leesanwei <lswei93@hotmail.com>
2016-04-26 23:32:56 +0200

    Printing a player name with a random score

--- JavaFinal/src/scores/TestHighScore1.java
+++ JavaFinal/src/scores/TestHighScore1.java
@@ -1,18 +1,44 @@
 package scores;
-
+ 
 import java.util.*;
 import java.io.*;
-
+ 
 public class TestHighScore1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Please write your player name below : ");
-		Scanner sc = new Scanner(System.in);
-		String namePlayer = sc.nextLine();
-		sc.close();
-		System.out.println(namePlayer);
-
-	}
-
+ 
+    public static void main(String[] args) {
+    	
+    	
+    	List<Integer> scores = new ArrayList<Integer>(); 
+    	Random rnd = new Random();
+    	Scanner sc = new Scanner(System.in);
+        // TODO Auto-generated method stub
+        System.out.println("Please write your player name below : ");
+        
+        String namePlayer = sc.nextLine();
+        sc.close();
+        //System.out.println(namePlayer);
+ 
+    
+ 
+        try{
+        	File file = new File("scoreSamples.txt");
+        	Scanner scan = new Scanner(file);
+        	while (scan.hasNextInt()) {
+                int i = scan.nextInt();
+                scores.add(i);
+                //System.out.println(i);
+            }
+            scan.close();
+            
+            int rndValue = rnd.nextInt(scores.size());
+            
+            Integer score = scores.get(rndValue);
+            System.out.println( namePlayer + " a fait un score de "+ score);
+        }
+        catch(IOException ex){
+            ex.printStackTrace();
+        }
+ 
+    }
+     
 }
